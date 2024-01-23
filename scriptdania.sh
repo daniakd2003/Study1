8e46f4787f0294ec65937c180ca36b0ca42387d1
dania <daniel20039335@gmail.com>
2024-01-23 18:23:05 +0000

    n

--- scriptdania.sh
+++ scriptdania.sh
@@ -13,19 +13,15 @@ read -p "Do you want to save your messag(y/n) " y
 if  [ $y=="y" ] 
 	then  echo "$x" > yourmessage.txt
 		echo "Your message is saved"
-else
-	 echo "message not saved"
+ 		read -p "Do you want to commit your message(y/n)? " g
+	if  [ $g=="y" ]
+		then    git add .
+			echo "files are added to repository"
+			read -p "enter comment: " c
+			git commit -m "$c"
+		else
+			echo "repository isn't updated"	
+	fi
+else 
+	echo "message not saved"
 fi
- 
-read -p "Do you want to commit your message(y/n)? " g
-
-
-if  [ $g=="y" ]
-	then    git add .
-		echo "files are added to repository"
-		read -p "enter comment: " c
-		git commit -m "$c"
-else
-		echo "repository isn't updated"
-fi
-
