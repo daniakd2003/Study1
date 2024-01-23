9854784ab045f4647166d80cb4d516efc790d8b0
dania <daniel20039335@gmail.com>
2024-01-23 18:25:14 +0000

    n

--- scriptdania.sh
+++ scriptdania.sh
@@ -10,11 +10,11 @@ echo "your message : $x"
 
 read -p "Do you want to save your messag(y/n) " y
 
-if  [ $y=="y" ] 
+if  [ "$y"=="y" ] 
 	then  echo "$x" > yourmessage.txt
 		echo "Your message is saved"
  		read -p "Do you want to commit your message(y/n)? " g
-	if  [ $g=="y" ]
+	if  [ "$g"=="y" ]
 		then    git add .
 			echo "files are added to repository"
 			read -p "enter comment: " c
