0f2a2c540ab985fcea05134481eaaf5df426c75f
dania <daniel20039335@gmail.com>
2024-01-23 18:30:59 +0000

    Added my bash script

--- scriptdania.sh
+++ scriptdania.sh
@@ -1,27 +1,27 @@
 
 #!/bin/bash
 
+read -p "Enter your message: " x
 
+echo "Your message: $x"
 
-read -p "Enter your message " x
+read -p "Do you want to save your message (y/n): " y
 
-echo "your message : $x"
-
-
-read -p "Do you want to save your messag(y/n) " y
-
-if  [ "$y"=="y" ] 
-	then  echo "$x" > yourmessage.txt
-		echo "Your message is saved"
- 		read -p "Do you want to commit your message(y/n)? " g
-	if  [ "$g"=="y" ]
-		then    git add .
-			echo "files are added to repository"
-			read -p "enter comment: " c
-			git commit -m "$c"
-		else
-			echo "repository isn't updated"	
-	fi
+if [ "$y" == "y" ]; then
+    echo "$x" > yourmessage.txt
+    echo "Your message is saved"
+    
+    read -p "Do you want to commit your message (y/n)? " g
+    
+    if [ "$g" == "y" ]; then
+        git add .
+        echo "Files are added to the repository"
+        read -p "Enter comment: " c
+        git commit -m "$c"
+    else
+        echo "Repository isn't updated"	
+    fi
 else 
-	echo "message not saved"
+    echo "Message not saved"
 fi
+
